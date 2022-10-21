1269ea81e532cdafaf9daab094d225c74a421ebc
OfficialFreak <npfreak@t-online.de>
2022-10-21 14:05:14 +0200

    Update post_picture.py

--- BeFake/post_picture.py
+++ BeFake/post_picture.py
@@ -13,4 +13,11 @@ with open("BeFake/pictures/front_camera.png", "rb") as f:
 
 now = pendulum.now()
 taken_at = f"{now.to_date_string()}T{now.to_time_string()}Z"
-print(bf.create_post(primary, secondary, False, False, "Greetings from Downtown Manhatten", {"latitude": "40.741895", "longitude": "-73.989308"}, 0, taken_at=taken_at))
+print(bf.create_post(primary, 
+                    secondary, 
+                    is_late=False, 
+                    is_public=False, 
+                    caption="🔭🌌", 
+                    location={"latitude": "-23.638909", "longitude": "-46.621997"}, 
+                    retakes=-2147483648,
+                    taken_at=taken_at))
